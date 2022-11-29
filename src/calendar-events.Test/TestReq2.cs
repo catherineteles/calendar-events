9a29ae28c534ed33f1da8a072f64ac8b9a65bec6
cathe <cacateles12@gmail.com>
2022-11-29 09:12:47 -0300

    Test: Tests for methods SearchByTitle and SearchByDate

--- src/calendar-events.Test/TestReq2.cs
+++ src/calendar-events.Test/TestReq2.cs
@@ -11,14 +11,24 @@ public class TestReq2
     [InlineData("Jogo da Copa", "2022-11-28", "Brasil vs Suiça", 0)]
     public void TestListSearchByTitle(string title, string date, string description,int expected)
     {
-        throw new NotImplementedException();
+        var newEvent = new Event(title, date, description);
+        var eventList = new EventList();
+
+        eventList.Add(newEvent);
+
+        eventList.SearchByTitle(title).Should().Be(expected);
     }
 
     [Theory(DisplayName = "Deve procurar um evento por data")]
     [InlineData("Jogo da Copa", "2022-11-28", "Brasil vs Suiça", 0)]
     public void TestListSearchByDate(string title, string date, string description, int expected)
     {
-        throw new NotImplementedException();
+        var newEvent = new Event(title, date, description);
+        var eventList = new EventList();
+
+        eventList.Add(newEvent);
+
+        eventList.SearchByDate(date).Should().Be(expected);
     }
 
     
